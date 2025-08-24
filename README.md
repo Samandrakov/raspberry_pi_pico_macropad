9dbde036f6952a999a8f3abc17c1e8bebafe9a1f
Danila Samandrakov <d.samandrakov@ispras.ru V>
2025-08-24 12:37:13 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # raspberry_pi_pico_macropad
 script for raspberry pi pico macropad
+
+1) Подключите pi pico к com порту зажав кнопку BOOTSEL на плате - появится директория для загрузки прошивки
+2) Загрузите файл из pico_driver в директорию
+3) После загрузки плата должна перезапуститься
+4) Используйте ide Thonny или любое другое удобное для circut python ide 
+5) Подключитесь к плате через ide 
+6) Выберите circutpython в настройках подключения
+7) Загрузите директорию adafruit_hid и скрипт macropad_script.py
+8) Переподключите плату без ide или иных подключений к com порту от каких либо ide
---
+++ macropad_script.py
@@ -0,0 +1,56 @@
+import time
+import board
+import digitalio
+import usb_hid
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keycode import Keycode
+
+# Инициализация клавиатуры
+keyboard = Keyboard(usb_hid.devices)
+
+# Настройка кнопок (GP0-GP8)
+buttons = []
+pins = [board.GP2, board.GP3, board.GP4, board.GP5,
+        board.GP6, board.GP7, board.GP8, board.GP9, board.GP10]
+
+# Клавиши для каждой кнопки (можно изменить)
+key_actions = [
+    Keycode.ONE,
+    Keycode.THREE,
+    Keycode.TWO,
+    Keycode.FOUR,
+    Keycode.SIX,
+    Keycode.FIVE,
+    Keycode.SEVEN,
+    Keycode.NINE,
+    Keycode.EIGHT
+
+]
+
+# Инициализация всех кнопок
+for pin in pins:
+    btn = digitalio.DigitalInOut(pin)
+    btn.direction = digitalio.Direction.INPUT
+    btn.pull = digitalio.Pull.UP
+    buttons.append(btn)
+
+# Для защиты от дребезга
+last_press_time = [0] * 9
+debounce_delay = 0.3
+
+while True:
+    for i in range(9):
+        if not buttons[i].value and (time.monotonic() - last_press_time[i] > debounce_delay):
+            action = key_actions[i]
+
+            # Обработка разных типов действий
+            if isinstance(action, list):  # Если это комбинация клавиш
+                keyboard.press(*action)
+            else:  # Если одиночная клавиша
+                keyboard.press(action)
+
+            keyboard.release_all()
+            last_press_time[i] = time.monotonic()
+
+            # Вывод отладочной информации
+            print(f"Button {i + 1} pressed")
